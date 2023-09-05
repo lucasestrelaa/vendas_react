60a709fa991fe740a5e47cd6fea557ba81cd8697
STATION018\Ti Desenvolvedor <analista19.ti@connectsa.com.br>
2023-09-05 17:23:07 -0300

    estilo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function App() {
-  const [produto, setProduto] = useState('');
-  const [preco, setPreco] = useState('');
+  const [produto, setProduto] = useState("");
+  const [preco, setPreco] = useState("");
 
   const [vendas, setVendas] = useState([]);
 
@@ -10,33 +10,48 @@ function App() {
     if (produto && preco) {
       const novaVenda = { produto, preco };
       setVendas([...vendas, novaVenda]);
-      setProduto('');
-      setPreco('');
+      setProduto("");
+      setPreco("");
     }
   };
 
   return (
     <div className="App">
-      <h1 className='text-4xl font-bold underline'>Sistema de Vendas</h1>
-
-      <div>
-        <input className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-          type="text"
-          placeholder="Produto"
-          value={produto}
-          onChange={(e) => setProduto(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Preço"
-          value={preco}
-          onChange={(e) => setPreco(e.target.value)}
-        />
-        <button onClick={adicionarVenda}>Adicionar Venda</button>
+      <div className="p-4 bg-blue-500">
+        <h1 className="text-4xl font-bold text-center">Teste</h1>
       </div>
 
-      <h2 className='text-2xl font-bold underline'>Vendas Registradas:</h2>
-      <ul>
+      <div className="text-center border">
+
+          <p>
+            <input
+              className="border m-2"
+              type="text"
+              placeholder="Produto"
+              value={produto}
+              onChange={(e) => setProduto(e.target.value)}
+            />
+          </p>
+          <p>
+            <input
+              className="border m-2"
+              type="text"
+              placeholder="Preço"
+              value={preco}
+              onChange={(e) => setPreco(e.target.value)}
+            />
+          </p>
+          <button
+            className="bg-blue-500 p-2 rounded-md"
+            onClick={adicionarVenda}
+          >
+            Adicionar Venda
+          </button>
+
+      </div>
+
+      <h2 className="text-2xl text-center">Vendas Registradas:</h2>
+      <ul className="text-center">
         {vendas.map((venda, index) => (
           <li key={index}>
             {venda.produto} - R${venda.preco}
